bff0090ce02e23861bad6cc9bbe13c1e54966869
cppdev <fckngRaccoon@yandex.ru>
2023-03-02 01:09:22 +0300

    Ex_02

---
+++ Ex_02/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите три числа: ");
+int FirstNumber = Console.Read();
+Console.Write(", ");
+int SecondNumber = Console.Read();
+Console.Write(", ");
+int ThirdNumber = Console.Read();
+Console.WriteLine(".");
+
+if (FirstNumber > SecondNumber && FirstNumber > ThirdNumber) // Первое наибольшее
+{
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(FirstNumber);
+}
+else if (SecondNumber > FirstNumber && SecondNumber > ThirdNumber) // Второе наибольшее
+{
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(SecondNumber);
+}
+else if (SecondNumber == FirstNumber && SecondNumber == ThirdNumber) // Равны
+{
+   Console.WriteLine("Числа равны!");
+}
+else // Третье наибольшее
+{
+    Console.Write("Наибольшее число: "); 
+    Console.WriteLine(ThirdNumber);
+}
